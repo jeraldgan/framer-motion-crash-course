f942f10483e88ef2eab837ff527d270416740e3b
Jerald Gan <jeraldgantc@gmail.com>
2024-05-13 15:11:07 +1000

    feat: add animation and comments while following along crash course video to show/hide button in BasicsOfMotion component

--- components/BasicsOfMotion.js
+++ components/BasicsOfMotion.js
@@ -13,39 +13,41 @@ const BasicsOfMotion = () => {
       }}
     >
       <motion.button
-        onClick={() => setIsVisible(!isVisible)}
-        className="example-button"
+        // This will help to animate the button when it is clicked
         layout
+        className="example-button"
+        onClick={() => setIsVisible(!isVisible)}
       >
         Show/Hide
       </motion.button>
       <AnimatePresence mode="popLayout">
         {isVisible && (
           <motion.div
+            style={{
+              width: "150px",
+              height: "150px",
+              background: "black",
+            }}
             initial={{
-              rotate: "0deg",
               scale: 0,
+              rotate: "0deg",
               y: 0,
             }}
             animate={{
-              rotate: "180deg",
               scale: 1,
+              rotate: "180deg",
               y: [0, 150, -150, -150, 0],
             }}
             exit={{
-              rotate: "0deg",
               scale: 0,
+              rotate: "0deg",
               y: 0,
             }}
             transition={{
               duration: 1,
               ease: "backInOut",
+              // times is used to define the time of the y animation at each point
               times: [0, 0.25, 0.5, 0.85, 1],
-            }}
-            style={{
-              width: 150,
-              height: 150,
-              background: "black",
             }}
           ></motion.div>
         )}
